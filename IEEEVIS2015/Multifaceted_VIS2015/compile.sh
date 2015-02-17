6a3a99b4366f525472a7916c3af3025488fecd37
rajincse <rajin.cse@gmail.com>
2015-02-16 22:12:23 -0500

    #1
    Paper structure uploaded

---
+++ IEEEVIS2015/Multifaceted_VIS2015/compile.sh
@@ -0,0 +1,6 @@
+latex Multifaceted_VIS2015
+bibtex Multifaceted_VIS2015
+latex Multifaceted_VIS2015
+latex Multifaceted_VIS2015
+dvips -t a4 -Ppdf -G0 -o Multifaceted_VIS2015.ps Multifaceted_VIS2015.dvi
+ps2pdf -dPDFSETTINGS=/prepress -dCompatibilityLevel=1.3 -dAutoFilterColorImages=false -dAutoFilterGrayImages=false -dColorImageFilter=/FlateEncode -dGrayImageFilter=/FlateEncode -dMonoImageFilter=/FlateEncode -dDownsampleColorImages=false -dDownsampleGrayImages=false EuroVis2015.ps Multifaceted_VIS2015.pdf
